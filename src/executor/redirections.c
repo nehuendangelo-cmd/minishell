8fb6c891f62f1035cc3229df23d6401fcd1ff6f5
Nehuen D'Angelo <nd-angel@f4r5s8.clusters.42paris.fr>
2026-02-25 19:14:07 +0100

    starting redirection handling in redirections.c

--- src/executor/redirections.c
+++ src/executor/redirections.c
@@ -6,7 +6,7 @@
 /*   By: nd-angel <nd-angel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 18:45:47 by nd-angel          #+#    #+#             */
-/*   Updated: 2026/02/25 18:46:28 by nd-angel         ###   ########.fr       */
+/*   Updated: 2026/02/25 19:06:10 by nd-angel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@ include "minishell.h"
 
 int    make_redirections(t_redir *redirs)
 {
+	int		fd;
 	
+	if (*redirs->type == REDIR_IN)
+	{
+		fd = open(file, O_RDONLY);
+		dup2(fd, STDIN_FILENO);
+	}
 }
