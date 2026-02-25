0d236dbd174e7eb1bc784f6bcf2f48dae7f71a1f
Nehuen D'Angelo <nd-angel@f4r5s8.clusters.42paris.fr>
2026-02-25 21:32:42 +0100

    continued redirections and made make.child

---
+++ src/executor/make.child.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   make.child.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nd-angel <nd-angel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/25 21:17:22 by nd-angel          #+#    #+#             */
+/*   Updated: 2026/02/25 21:29:19 by nd-angel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	make_child(t_cmd *cmd, char **envp)
+{
+	char	*cmd_path;
+	if (make_redirections(cmd->redirs) == -1)
+		exit(1);
+	cmd_path = find_cmdpath(cmd->cmd_and_args[0], envp);
+	if (cmd_path == NULL)
+	{
+		ft_putstr_fd(cmd->cmd_and_args[0], 2);
+      	ft_putendl_fd(": command not found", 2);
+		exit(127);
+	}
+	execve(cmd_path, cmd->cmd_and_args, envp);
+	free(cmd_path);
+	perror(cmd->cmd_and_args[0]);
+	exit(127);
+}
--- src/executor/redirections.c
+++ src/executor/redirections.c
@@ -6,19 +6,42 @@
 /*   By: nd-angel <nd-angel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 18:45:47 by nd-angel          #+#    #+#             */
-/*   Updated: 2026/02/25 19:06:10 by nd-angel         ###   ########.fr       */
+/*   Updated: 2026/02/25 21:12:43 by nd-angel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-include "minishell.h"
+#include "minishell.h"
 
 int    make_redirections(t_redir *redirs)
 {
 	int		fd;
 	
-	if (*redirs->type == REDIR_IN)
+	while (redirs)
 	{
-		fd = open(file, O_RDONLY);
-		dup2(fd, STDIN_FILENO);
+		fd = -1;
+		if (redirs->type == REDIR_IN)
+		{
+			fd = open(redirs->file, O_RDONLY);
+			if (fd == -1)
+			return (perror("Error opening infile"), -1);
+			dup2(fd, STDIN_FILENO);
+		}
+		if (redirs->type == REDIR_OUT)
+		{
+			fd = open(redirs->file, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+			if (fd == -1)
+				return (perror("Error opening infile"), -1);
+			dup2(fd, STDOUT_FILENO);
+		}
+		if (redirs->type == REDIR_APPEND)
+		{
+			fd = open(redirs->file, O_WRONLY|O_CREAT|O_APPEND, 0644);
+			if (fd == -1)
+				return (perror("Error opening infile"), -1);
+			dup2(fd, STDOUT_FILENO);
+		}
+		close(fd);
+		redirs = redirs->next;
 	}
+	return (0);
 }
