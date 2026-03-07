e983d3cfb2b84165ec0022309101ee8299f547e6
Nehuen D'Angelo <nd-angel@f4r4s12.clusters.42paris.fr>
2026-03-07 20:36:03 +0100

    w

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: nd-angel <nd-angel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 00:07:05 by nehuen            #+#    #+#             */
-/*   Updated: 2026/03/07 19:10:14 by nd-angel         ###   ########.fr       */
+/*   Updated: 2026/03/07 20:34:04 by nd-angel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,7 +45,7 @@ int	main(int argc, char **argv, char **envp)
 	signal(SIGQUIT, SIG_IGN);
 	shell.envp = ft_copy_env(envp);
 	shell.last_exit = 0;
-	(void)shell
+	(void)shell;
 	while (1)
 	{
 		line = readline("minishell$");
